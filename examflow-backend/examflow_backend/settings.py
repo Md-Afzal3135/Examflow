3b7a9926415318ea7f09ef363fc2f62d725cd850
Md-Afzal3135 <amohd3135@gmail.com>
2026-06-01 01:34:20 +0530

    Disable debug mode for production

--- examflow-backend/examflow_backend/settings.py
+++ examflow-backend/examflow_backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-change-in-prod')
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
